593e74de07cb155a348d41423d44848912fc3300
Indra Rudianto <indrarudianto.official@gmail.com>
2024-06-14 09:27:37 +0700

    refactor: Add DataStore class for managing data storage

---
+++ packages/charts/src/data/dataStore.ts
@@ -0,0 +1,41 @@
+export class DataStore<T> {
+  private data: Map<string, T> = new Map();
+
+  constructor() {}
+
+  set(key: string, data: T): void {
+    this.data.set(key, data);
+  }
+
+  get(key: string): T | undefined {
+    return this.data.get(key);
+  }
+
+  has(key: string): boolean {
+    return this.data.has(key);
+  }
+
+  remove(key: string): void {
+    this.data.delete(key);
+  }
+
+  clear(): void {
+    this.data.clear();
+  }
+
+  size(): number {
+    return this.data.size;
+  }
+
+  keys(): IterableIterator<string> {
+    return this.data.keys();
+  }
+
+  values(): IterableIterator<T> {
+    return this.data.values();
+  }
+
+  entries(): IterableIterator<[string, T]> {
+    return this.data.entries();
+  }
+}
