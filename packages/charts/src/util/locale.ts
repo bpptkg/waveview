a82e0aa707aff5a0f552ffe7647b59b898a8dec6
Indra Rudianto <indrarudianto.official@gmail.com>
2024-06-17 19:51:25 +0700

    feat: Add Indonesian (id_ID) locale support

--- packages/charts/src/util/locale.ts
+++ packages/charts/src/util/locale.ts
@@ -1,4 +1,5 @@
 import en_US from "../i18n/en_US";
+import id_ID from "../i18n/id_ID";
 
 export interface Locale {
   time: {
@@ -17,6 +18,8 @@ export function getLocale(lang: string): Locale {
   switch (lang) {
     case "en_US":
       return en_US;
+    case "id_ID":
+      return id_ID;
     default:
       return en_US;
   }
