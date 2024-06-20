a5437fd18db38ffca83e1fbab2e8b2619484682f
Indra Rudianto <indrarudianto.official@gmail.com>
2024-06-20 14:11:57 +0700

    refactor: Add SeismogramChart component and integrate it into Picker view

--- packages/web/src/views/picker/Picker.tsx
+++ packages/web/src/views/picker/Picker.tsx
@@ -1,14 +1,20 @@
 import HelicorderChart from '../../components/Chart/HelicorderChart';
 import PickerMenu from '../../components/PickerMenu/PickerMenu';
 import HelicorderToolbar from '../../components/Toolbar/HelicorderToolbar';
+import SeismogramChart from '../../components/Chart/SeismogramChart';
 
 const Picker = () => {
   return (
     <>
       <PickerMenu />
       <HelicorderToolbar />
-      <div className="flex-grow relative mt-1">
-        <HelicorderChart />
+      <div className="flex-grow relative mt-1 flex">
+        <div className="relative w-1/3">
+          <HelicorderChart />
+        </div>
+        <div className="relative w-2/3">
+          <SeismogramChart />
+        </div>
       </div>
     </>
   );
