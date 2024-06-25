9208a27658ba79eae5e969d4c37816f5d06f5748
Indra Rudianto <indrarudianto.official@gmail.com>
2024-06-25 11:52:29 +0700

    refactor: Update SearchBox component to use makeStyles for styling

--- packages/web/src/components/Header/SearchBox.tsx
+++ packages/web/src/components/Header/SearchBox.tsx
@@ -1,9 +1,14 @@
-import { SearchBox as FluentSearchBox } from '@fluentui/react-components';
+import { SearchBox as FluentSearchBox, makeStyles } from '@fluentui/react-components';
+
+const useStyles = makeStyles({
+  searchBox: {
+    width: '264px',
+  },
+});
 
 const SearchBox = () => {
-  return (
-      <FluentSearchBox appearance='filled-lighter' placeholder='Search' />
-  );
+  const styles = useStyles();
+  return <FluentSearchBox appearance="filled-lighter" placeholder="Search" className={styles.searchBox} />;
 };
 
 export default SearchBox;
