8796d5ebeafbcd51b6fed485ef5108764232ec43
Indra Rudianto <indrarudianto.official@gmail.com>
2024-07-01 23:34:30 +0700

    refactor: Update EventEmitter class to use on, off, and emit methods

--- packages/charts/src/event/eventEmitter.ts
+++ packages/charts/src/event/eventEmitter.ts
@@ -3,33 +3,23 @@ import { EventMap } from "../util/types";
 export class EventEmitter<E extends EventMap = EventMap> {
   private _listeners: { [K in keyof E]?: E[K][] } = {};
 
-  addEventListener<K extends keyof E>(event: K, fn: E[K]): void {
+  on<K extends keyof E>(event: K, fn: E[K]): void {
     if (!this._listeners[event]) {
       this._listeners[event] = [];
     }
     this._listeners[event]?.push(fn);
   }
 
-  removeEventListener<K extends keyof E>(event: K, fn: E[K]): void {
+  off<K extends keyof E>(event: K, fn: E[K]): void {
     if (this._listeners[event]) {
       this._listeners[event] = this._listeners[event]?.filter((f) => f !== fn);
     }
   }
 
-  dispatchEvent<K extends keyof E>(event: K, ...args: Parameters<E[K]>): void {
+  emit<K extends keyof E>(event: K): void;
+  emit<K extends keyof E>(event: K, ...args: Parameters<E[K]>): void;
+  emit<K extends keyof E>(event: K, ...args: any[]): void {
     this._listeners[event]?.forEach((fn) => fn.apply(this, args));
-  }
-
-  on<K extends keyof E>(event: K, fn: E[K]): void {
-    this.addEventListener(event, fn);
-  }
-
-  off<K extends keyof E>(event: K, fn: E[K]): void {
-    this.removeEventListener(event, fn);
-  }
-
-  emit<K extends keyof E>(event: K, ...args: Parameters<E[K]>): void {
-    this.dispatchEvent(event, ...args);
   }
 
   once<K extends keyof E>(event: K, fn: E[K]): void {
