cc621f5f4004efcb505cd108e0cb777404b31103
Indra Rudianto <indrarudianto.official@gmail.com>
2024-07-02 10:20:35 +0700

    refactor: Add ThemeManager class for managing themes

---
+++ packages/charts/src/theme/themeManager.ts
@@ -0,0 +1,38 @@
+import { ThemeName, ThemeRegistry, ThemeStyle } from "../util/types";
+import darkTheme from "./dark";
+import lightTheme from "./light";
+
+export class ThemeManager {
+  private static _instance: ThemeManager;
+  private _registry: ThemeRegistry = {
+    light: lightTheme,
+    dark: darkTheme,
+  };
+
+  static getInstance(): ThemeManager {
+    if (!ThemeManager._instance) {
+      ThemeManager._instance = new ThemeManager();
+    }
+    return ThemeManager._instance;
+  }
+
+  getTheme(name: ThemeName): ThemeStyle {
+    const theme = this._registry[name];
+    if (!theme) {
+      throw new Error(`Theme '${name}' is not registered.`);
+    }
+    return theme;
+  }
+
+  registerTheme(name: string, theme: ThemeStyle): void {
+    this._registry[name] = theme;
+  }
+}
+
+export function getThemeManager(): ThemeManager {
+  return ThemeManager.getInstance();
+}
+
+export function registerTheme(name: string, theme: ThemeStyle): void {
+  getThemeManager().registerTheme(name, theme);
+}
