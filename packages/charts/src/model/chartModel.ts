9ebbe1ae7a31971f15eae1ba41e2f1a52c38002c
Indra Rudianto <indrarudianto.official@gmail.com>
2024-07-02 10:20:03 +0700

    refactor: Update ChartModel to include all ChartOptions properties

--- packages/charts/src/model/chartModel.ts
+++ packages/charts/src/model/chartModel.ts
@@ -2,26 +2,18 @@ import { ModelOptions } from "../util/types";
 import { Model } from "./model";
 
 export interface ChartOptions extends ModelOptions {
-  backgroundColor?: string;
-  devicePixelRatio?: number;
-  darkMode?: boolean;
-  autoDensity?: boolean;
-  antialias?: boolean;
-  resizeTo?: HTMLElement;
+  backgroundColor: string;
+  devicePixelRatio: number;
+  darkMode: boolean;
+  autoDensity: boolean;
+  antialias: boolean;
 }
 
 export class ChartModel<T extends ChartOptions> extends Model<T> {
   override readonly type = "chart";
 
-  static defaultOptions: ChartOptions = {
-    backgroundColor: "transparent",
-    darkMode: false,
-    autoDensity: true,
-    antialias: true,
-  };
-
   constructor(options?: T) {
-    const opts = { ...ChartModel.defaultOptions, ...options };
+    const opts = { ...options };
     super(opts as T);
   }
 }
