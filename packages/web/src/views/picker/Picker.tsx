cd2885f595ba916b622734837f9dbfc14315411f
Indra Rudianto <indrarudianto.official@gmail.com>
2024-08-10 16:20:58 +0700

    refactor: Remove PickerMenu component from Picker.tsx

--- packages/web/src/views/picker/Picker.tsx
+++ packages/web/src/views/picker/Picker.tsx
@@ -1,10 +1,8 @@
 import HelicorderWorkspace from '../../components/Picker/PickerWorkspace';
-import PickerMenu from '../../components/Picker/PickerMenu/PickerMenu';
 
 const Picker = () => {
   return (
     <>
-      <PickerMenu />
       <HelicorderWorkspace />
     </>
   );
