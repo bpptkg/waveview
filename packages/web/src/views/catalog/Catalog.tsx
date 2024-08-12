32762ee4e7bd07b55e536a2f6a250ad25c9a1430
Indra Rudianto <indrarudianto.official@gmail.com>
2024-08-12 20:29:27 +0700

    refactor: Update Catalog component to use location.pathname for defaultSelectedValue in TabList

--- packages/web/src/views/catalog/Catalog.tsx
+++ packages/web/src/views/catalog/Catalog.tsx
@@ -1,9 +1,10 @@
 import { Tab, TabList, TabListProps } from '@fluentui/react-components';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import CatalogPicker from '../../components/Catalog/CatalogPicker';
 
 const Catalog = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleTabSelect: TabListProps['onTabSelect'] = (_, data) => {
     navigate(data.value as string);
@@ -11,7 +12,7 @@ const Catalog = () => {
 
   return (
     <div>
-      <TabList onTabSelect={handleTabSelect} defaultSelectedValue={'/catalog/events'}>
+      <TabList onTabSelect={handleTabSelect} defaultSelectedValue={location.pathname}>
         <div className="flex justify-between items-center w-full">
           <div className="flex">
             <Tab value="/catalog/events">Events</Tab>
