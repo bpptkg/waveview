75881fd7e71219f4479d0ab323efd4ef5bef347a
Indra Rudianto <indrarudianto.official@gmail.com>
2024-08-12 20:07:16 +0700

    refactor: Add user avatar to Account component in Header

--- packages/web/src/components/Header/Account.tsx
+++ packages/web/src/components/Header/Account.tsx
@@ -3,7 +3,7 @@ import { useUserStore } from '../../stores/user';
 
 const Account = () => {
   const { user } = useUserStore();
-  return <Avatar color="colorful" name={user?.name ?? user?.username} />;
+  return <Avatar color="colorful" name={user?.name ?? user?.username} image={{ src: user?.avatar }} />;
 };
 
 export default Account;
