f8fac422f32f80d5d2d7738db5385d5628ea4ece
Indra Rudianto <indrarudianto.official@gmail.com>
2024-08-13 20:13:49 +0700

    refactor: Update OrganizationPicker component for displaying organization name

--- packages/web/src/components/Header/Header.tsx
+++ packages/web/src/components/Header/Header.tsx
@@ -1,19 +1,14 @@
 import { Switch } from '@fluentui/react-components';
 import React, { useCallback } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
 import { useAppStore } from '../../stores/app';
+import OrganizationPicker from '../Organization/OrganizationPicker';
 import VolcanoPicker from '../Volcano/VolcanoPicker';
 import Account from './Account';
 import ArrowNavigation from './ArrowNavigation';
-import LogoImage from './LogoImage';
-import LogoText from './LogoText';
 import Notification from './Notification';
 import SearchBox from './SearchBox';
 
 const Header = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-
   const { darkMode, toggleTheme } = useAppStore();
 
   const handleToggleDarkMode = useCallback(
@@ -26,19 +21,8 @@ const Header = () => {
 
   return (
     <div className="flex h-[48px] items-center justify-between px-2">
-      <div className="flex items-center gap-2">
-        <a
-          className="flex items-center gap-3 cursor-pointer"
-          onClick={() => {
-            if (location.pathname !== '/' && location.pathname !== '/picker') {
-              navigate('/');
-            }
-          }}
-        >
-          <LogoImage />
-          <LogoText />
-        </a>
-
+      <div className="flex items-center">
+        <OrganizationPicker />
         <VolcanoPicker />
       </div>
 
---
+++ packages/web/src/components/Organization/OrganizationPicker.tsx
@@ -0,0 +1,83 @@
+import { Button, Image, makeStyles, MenuButton, MenuItem, MenuList, Popover, PopoverProps, PopoverSurface, PopoverTrigger } from '@fluentui/react-components';
+import { Checkmark20Regular, Dismiss16Regular } from '@fluentui/react-icons';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useOrganizationStore } from '../../stores/organization';
+import LogoImage from '../Header/LogoImage';
+
+const useOrganizationPickerStyles = makeStyles({
+  popoverSurface: {
+    padding: '8px',
+  },
+});
+
+const OrganizationPicker = () => {
+  const { currentOrganization, allOrganizations, setCurrentOrganization } = useOrganizationStore();
+  const navigate = useNavigate();
+  const styles = useOrganizationPickerStyles();
+  const [open, setOpen] = useState(false);
+  const handleOpenChange: PopoverProps['onOpenChange'] = (_, data) => setOpen(data.open || false);
+
+  return (
+    <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2">
+        <a
+          className="flex items-center gap-3 cursor-pointer"
+          onClick={() => {
+            if (location.pathname !== '/' && location.pathname !== '/picker') {
+              navigate('/');
+            }
+          }}
+        >
+          <LogoImage />
+        </a>
+      </div>
+
+      <Popover open={open} onOpenChange={handleOpenChange}>
+        <PopoverTrigger disableButtonEnhancement>
+          <MenuButton size="medium" appearance="transparent">
+            <span className="font-semibold text-black dark:text-white">{currentOrganization?.name}</span>
+          </MenuButton>
+        </PopoverTrigger>
+
+        <PopoverSurface tabIndex={-1} className={styles.popoverSurface}>
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center justify-between gap-2">
+              <span className="text-xs font-semibold">Switch organization context</span>
+              <Button appearance="transparent" icon={<Dismiss16Regular />} onClick={() => setOpen(false)} />
+            </div>
+            <MenuList>
+              {allOrganizations.map((org) => (
+                <MenuItem
+                  key={org.id}
+                  onClick={() => {
+                    setCurrentOrganization(org);
+                    setOpen(false);
+                  }}
+                  icon={org.id === currentOrganization?.id ? <Checkmark20Regular /> : undefined}
+                >
+                  <div className="flex flex-col gap-1">
+                    <div className="flex items-center gap-2">
+                      {org?.avatar && (
+                        <div>
+                          <div className="w-[24px] h-[24px]">
+                            <Image alt={org.name} src={org.avatar} fit="cover" shape="circular" />
+                          </div>
+                        </div>
+                      )}
+                      <div className="flex gap-2">
+                        <span className="font-semibold">{org.name}</span>
+                      </div>
+                    </div>
+                  </div>
+                </MenuItem>
+              ))}
+            </MenuList>
+          </div>
+        </PopoverSurface>
+      </Popover>
+    </div>
+  );
+};
+
+export default OrganizationPicker;
