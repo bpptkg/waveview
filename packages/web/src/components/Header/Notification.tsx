ba8e44acc72a635b918bdb74b468084727129872
Indra Rudianto <indrarudianto.official@gmail.com>
2024-08-15 17:53:49 +0700

    refactor: Update notification popover to Header component

--- packages/web/src/components/Header/Notification.tsx
+++ packages/web/src/components/Header/Notification.tsx
@@ -1,8 +1,29 @@
-import { Button } from '@fluentui/react-components';
-import { Alert20Regular } from '@fluentui/react-icons';
+import { Button, makeStyles, Popover, PopoverProps, PopoverSurface, PopoverTrigger } from '@fluentui/react-components';
+import { AlertRegular } from '@fluentui/react-icons';
+import { useState } from 'react';
+
+const useNotificationStyles = makeStyles({
+  popoverSurface: {
+    padding: '8px',
+  },
+});
 
 const Notification = () => {
-  return <Button size="small" appearance="transparent" icon={<Alert20Regular />} />;
+  const styles = useNotificationStyles();
+  const [open, setOpen] = useState(false);
+  const handleOpenChange: PopoverProps['onOpenChange'] = (_, data) => setOpen(data.open || false);
+
+  return (
+    <Popover open={open} onOpenChange={handleOpenChange} withArrow>
+      <PopoverTrigger disableButtonEnhancement>
+        <Button size="small" appearance="transparent" icon={<AlertRegular fontSize={20} />} />
+      </PopoverTrigger>
+
+      <PopoverSurface tabIndex={-1} className={styles.popoverSurface}>
+        <div>This notification popover.</div>
+      </PopoverSurface>
+    </Popover>
+  );
 };
 
 export default Notification;
