bfff92d38db5a306702e581a328d50e8a9b0accd
Indra Rudianto <indrarudianto.official@gmail.com>
2024-08-15 20:30:00 +0700

    refactor: Update Catalog component to use selectedTab state

--- packages/web/src/views/catalog/Catalog.tsx
+++ packages/web/src/views/catalog/Catalog.tsx
@@ -1,18 +1,24 @@
 import { Tab, TabList, TabListProps } from '@fluentui/react-components';
+import { useEffect, useState } from 'react';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import CatalogPicker from '../../components/Catalog/CatalogPicker';
 
 const Catalog = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const [selectedTab, setSelectedTab] = useState(location.pathname);
 
   const handleTabSelect: TabListProps['onTabSelect'] = (_, data) => {
     navigate(data.value as string);
   };
 
+  useEffect(() => {
+    setSelectedTab(location.pathname);
+  }, [location.pathname]);
+
   return (
     <>
-      <TabList onTabSelect={handleTabSelect} defaultSelectedValue={location.pathname}>
+      <TabList onTabSelect={handleTabSelect} selectedValue={selectedTab}>
         <div className="flex justify-between items-center w-full">
           <div className="flex">
             <Tab value="/catalog/events">Events</Tab>
