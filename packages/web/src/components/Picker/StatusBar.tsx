c3f276d0ea128ff0b0fbd2ae8d96652ac7b4aa17
Indra Rudianto <indrarudianto.official@gmail.com>
2024-08-16 22:39:37 +0700

    refactor: Update StatusBar component styling for better alignment

--- packages/web/src/components/Picker/StatusBar.tsx
+++ packages/web/src/components/Picker/StatusBar.tsx
@@ -4,7 +4,7 @@ import TimeZoneSelector from './TimezoneSelector';
 
 const StatusBar = () => {
   return (
-    <div className="bg-white dark:bg-black relative flex items-center justify-between gap-2 mr-2 h-[20px]">
+    <div className="bg-white dark:bg-black relative flex items-center justify-between gap-2 px-2 h-[20px]">
       <div>
         <TimeRangeLabel />
       </div>
