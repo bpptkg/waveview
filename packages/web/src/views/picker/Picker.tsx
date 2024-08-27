790806eeaddd1ae3b6148b4f13de24b3704be44e
Indra Rudianto <indrarudianto.official@gmail.com>
2024-08-27 18:56:06 +0700

    refactor: Update PickerWorkspace to remove showMarkersOnReady prop

--- packages/web/src/views/picker/Picker.tsx
+++ packages/web/src/views/picker/Picker.tsx
@@ -3,7 +3,7 @@ import PickerWorkspace from '../../components/Picker/PickerWorkspace';
 const Picker = () => {
   return (
     <>
-      <PickerWorkspace showMarkersOnReady />
+      <PickerWorkspace />
     </>
   );
 };
