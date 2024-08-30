53ed01a2b188355729a12f5bd21d1169b464792e
Indra Rudianto <indrarudianto.official@gmail.com>
2024-08-30 20:28:21 +0700

    feat: Add close button to EventDetailEditor

--- packages/web/src/views/catalog/EventDetailEditor.tsx
+++ packages/web/src/views/catalog/EventDetailEditor.tsx
@@ -1,4 +1,4 @@
-import { FluentProvider, webDarkTheme, webLightTheme } from '@fluentui/react-components';
+import { Button, FluentProvider, webDarkTheme, webLightTheme } from '@fluentui/react-components';
 import React, { useCallback } from 'react';
 import ReactDOM from 'react-dom';
 import PickerWorkspace from '../../components/Picker/PickerWorkspace';
@@ -29,6 +29,12 @@ const EventDetailEditor: React.FC<EventDetailEditorProps> = (props) => {
   return ReactDOM.createPortal(
     <FluentProvider theme={darkMode ? webDarkTheme : webLightTheme}>
       <div className="w-screen h-screen absolute top-0 right-0 bottom-0 left-0 z-[9999] bg-neutral-grey-98 dark:bg-neutral-grey-12 flex flex-col">
+        <div className="flex items-center justify-between relative px-2 py-1">
+          <div className="text-md font-semibold">Edit Event</div>
+          <Button appearance="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </div>
         <div className="flex flex-col flex-grow relative">
           <PickerWorkspace event={event} showHelicorder={false} showEventMarkers={false} showSeismogram={true} onSave={handleSave} onCancel={handleClose} />
         </div>
