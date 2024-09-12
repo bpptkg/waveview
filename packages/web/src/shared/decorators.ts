717d8e5101f842aaa9d4d51a752ab5a46dcbaa65
Indra Rudianto <indrarudianto.official@gmail.com>
2024-09-12 21:13:14 +0700

    Refactor shared decorators to add Debounce decorator

---
+++ packages/web/src/shared/decorators.ts
@@ -0,0 +1,14 @@
+export function Debounce(delay: number): MethodDecorator {
+  return function (_target: any, _propertyKey: string | symbol, descriptor: PropertyDescriptor) {
+    let timeout: any = null;
+
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function (...args: any[]) {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => originalMethod.apply(this, args), delay);
+    };
+
+    return descriptor;
+  };
+}
