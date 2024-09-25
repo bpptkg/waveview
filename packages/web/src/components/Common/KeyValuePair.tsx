e8da3bb25e280c0c465a5d212206a093f439b47f
Indra Rudianto <indrarudianto.official@gmail.com>
2024-09-25 13:45:50 +0700

    Refactor KeyValuePair component to support column layout

--- packages/web/src/components/Common/KeyValuePair.tsx
+++ packages/web/src/components/Common/KeyValuePair.tsx
@@ -3,13 +3,20 @@ import React from 'react';
 interface KeyValuePairProps {
   label: string;
   value: React.ReactNode;
+  column?: boolean;
 }
 
-const KeyValuePair: React.FC<KeyValuePairProps> = ({ label, value }) => (
-  <div className="flex items-center justify-between hover:bg-gray-100 dark:hover:bg-gray-800">
-    <div>{label}</div>
-    <div>{value}</div>
-  </div>
-);
+const KeyValuePair: React.FC<KeyValuePairProps> = ({ label, value, column }) =>
+  column ? (
+    <div className="flex flex-col gap-2">
+      <div>{label}</div>
+      <div>{value}</div>
+    </div>
+  ) : (
+    <div className="flex items-center justify-between hover:bg-gray-100 dark:hover:bg-gray-800">
+      <div>{label}</div>
+      <div>{value}</div>
+    </div>
+  );
 
 export default KeyValuePair;
