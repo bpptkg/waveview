e8174d34ded14260edf5120ed698f1f4168f392b
Indra Rudianto <indrarudianto.official@gmail.com>
2024-09-26 20:29:50 +0700

    Update react-echarts package

--- packages/react-echarts/src/ReactEcharts.tsx
+++ packages/react-echarts/src/ReactEcharts.tsx
@@ -1,8 +1,13 @@
 import { ECharts, EChartsOption, init } from "echarts";
-import React, { CSSProperties, useEffect, useRef } from "react";
+import React, {
+  CSSProperties,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from "react";
 
 export interface ReactEChartsProps {
-  option: EChartsOption;
+  option?: EChartsOption;
   className?: string;
   style?: CSSProperties;
   notMerge?: boolean;
@@ -12,9 +17,29 @@ export interface ReactEChartsProps {
   autoResize?: boolean;
 }
 
-export const ReactECharts: React.FC<ReactEChartsProps> = (
-  props: ReactEChartsProps
-) => {
+export interface ReactEChartsType {
+  getInstance: () => ECharts;
+  setOption: (
+    option: EChartsOption,
+    notMerge?: boolean,
+    lazyUpdate?: boolean
+  ) => void;
+  clear: () => void;
+  resize: () => void;
+  showLoading: () => void;
+  hideLoading: () => void;
+  dispose: () => void;
+  toDataURL: (options?: {
+    type?: "png" | "jpeg" | "svg";
+    backgroundColor?: string;
+    pixelRatio?: number;
+    excludeComponents?: string[];
+  }) => string | undefined;
+}
+
+export const ReactECharts: React.ForwardRefExoticComponent<
+  ReactEChartsProps & React.RefAttributes<ReactEChartsType>
+> = React.forwardRef((props, ref) => {
   const {
     option,
     className,
@@ -28,10 +53,44 @@ export const ReactECharts: React.FC<ReactEChartsProps> = (
   const containerRef = useRef<HTMLDivElement>(null);
   const echartInstanceRef = useRef<ECharts | null>(null);
 
+  useImperativeHandle(ref, () => ({
+    getInstance: () => echartInstanceRef.current!,
+    setOption: (
+      option: EChartsOption,
+      notMerge?: boolean,
+      lazyUpdate?: boolean
+    ) => {
+      echartInstanceRef.current?.setOption(option, notMerge, lazyUpdate);
+    },
+    clear: () => {
+      echartInstanceRef.current?.clear();
+    },
+    resize: () => {
+      echartInstanceRef.current?.resize();
+    },
+    showLoading: () => {
+      echartInstanceRef.current?.showLoading();
+    },
+    hideLoading: () => {
+      echartInstanceRef.current?.hideLoading();
+    },
+    dispose: () => {
+      echartInstanceRef.current?.dispose();
+    },
+    toDataURL: (options?: {
+      type?: "png" | "jpeg" | "svg";
+      backgroundColor?: string;
+      pixelRatio?: number;
+      excludeComponents?: string[];
+    }) => {
+      return echartInstanceRef.current?.getDataURL(options);
+    },
+  }));
+
   useEffect(() => {
     if (containerRef.current) {
       echartInstanceRef.current = init(containerRef.current);
-      echartInstanceRef.current.setOption(option, notMerge, lazyUpdate);
+      echartInstanceRef.current.setOption(option || {}, notMerge, lazyUpdate);
       if (showLoading) echartInstanceRef.current.showLoading(loadingOption);
       else echartInstanceRef.current.hideLoading();
       if (autoResize) {
@@ -56,4 +115,4 @@ export const ReactECharts: React.FC<ReactEChartsProps> = (
   }, [option, notMerge, lazyUpdate, showLoading, loadingOption, autoResize]);
 
   return <div ref={containerRef} style={style} className={className}></div>;
-};
+});
--- packages/react-echarts/src/index.ts
+++ packages/react-echarts/src/index.ts
@@ -1,17 +1,3 @@
-import * as echarts from "echarts";
-import { EChartsOption } from "echarts";
-import ReactEChartsCore from "./core";
-import type { ReactEChartsProps } from "./types";
-
 export { createGrid, makeIndex, type GridOptions } from "./helper/grid";
-export type { EChartsOption, ReactEChartsProps as EChartsReactProps };
-
-// export the Component the echarts Object.
-export class ReactECharts extends ReactEChartsCore {
-  constructor(props: ReactEChartsProps) {
-    super(props);
-
-    // 初始化为 echarts 整个包
-    this.echarts = echarts;
-  }
-}
+export { ReactECharts } from "./ReactEcharts";
+export type { ReactEChartsProps, ReactEChartsType } from "./ReactEcharts";
