bf799f9cfe0bc01d5c52373210a98c116f032c0c
Indra Rudianto <indrarudianto.official@gmail.com>
2024-09-30 20:26:17 +0700

    Refactor grid.ts to add utility functions for creating grid layouts and generating an array of indices.

--- packages/react-echarts/src/helper/grid.ts
+++ packages/react-echarts/src/helper/grid.ts
@@ -6,8 +6,17 @@ export interface GridOptions {
   left?: number;
 }
 
+/**
+ * Create an array of indices from 0 to n - 1.
+ */
 export const makeIndex = (n: number) => Array.from({ length: n }, (_, i) => i);
 
+/**
+ * Create a grid layout for n rows.
+ *
+ * @param nrows The number of rows.
+ * @param options The grid options.
+ */
 export const createGrid = (nrows: number, options: GridOptions = {}) => {
   const { margin = 2, top = 5, right = 10, bottom = 5, left = 10 } = options;
   const availableSpace = 100 - (top + bottom);
