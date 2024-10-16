cdbbf9a446a7df8ad8d0e5f43f22ae3e048a77e3
Indra Rudianto <indrarudianto.official@gmail.com>
2024-10-16 21:37:54 +0700

    Update LoadingIndicator component in Picker to use Fluent UI Spinner for loading animation

--- packages/web/src/components/Picker/Statusbar/LoadingIndicator.tsx
+++ packages/web/src/components/Picker/Statusbar/LoadingIndicator.tsx
@@ -1,10 +1,11 @@
+import { Spinner } from '@fluentui/react-components';
 import React from 'react';
 import { usePickerStore } from '../../../stores/picker';
 
 const LoadingIndicator: React.FC = () => {
   const { loading } = usePickerStore();
   if (!loading) return null;
-  return <span className="text-xs dark:text-neutral-grey-84">Loading...</span>;
+  return <Spinner label={<span className="text-xs dark:text-neutral-grey-84">Loading...</span>} size="extra-tiny" />;
 };
 
 export default LoadingIndicator;
