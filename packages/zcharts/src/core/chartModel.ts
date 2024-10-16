1fbf5ddc231b05cb879453295207a5717d04bdc3
Indra Rudianto <indrarudianto.official@gmail.com>
2024-10-16 20:52:07 +0700

    Add useOffscreenRendering in ChartOptions

--- packages/zcharts/src/core/chartModel.ts
+++ packages/zcharts/src/core/chartModel.ts
@@ -2,8 +2,22 @@ import { ModelOptions } from "../util/types";
 import { Model } from "./model";
 
 export interface ChartOptions extends ModelOptions {
+  /**
+   * The background color of the chart.
+   */
   backgroundColor: string;
+  /**
+   * The color of the chart border.
+   */
   darkMode: boolean;
+  /**
+   * Use offscreen rendering for the helicorder chart. This is useful for
+   * rendering large amounts of data in a separate thread. Offscreen rendering
+   * only renders signal data and does not render any other chart elements. It
+   * is only supported in modern browsers and requires a web worker to be
+   * enabled.
+   */
+  useOffscrrenRendering: boolean;
 }
 
 export class ChartModel<T extends ChartOptions> extends Model<T> {
