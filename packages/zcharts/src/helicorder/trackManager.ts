f9194841b2229beea7a76b4ae628e1529518ae7d
Indra Rudianto <indrarudianto.official@gmail.com>
2024-10-18 13:39:23 +0700

    Add docs to helicorder track manager

--- packages/zcharts/src/helicorder/trackManager.ts
+++ packages/zcharts/src/helicorder/trackManager.ts
@@ -10,57 +10,75 @@ export interface SetTrackDataOptions {
   range: [number, number];
 }
 
+/**
+ * TrackManager manages the tracks in the helicorder.
+ */
 export class TrackManager {
   private helicorder: Helicorder;
-  private _tracks: Map<string, TrackView> = new Map();
+  private trackStore: Map<string, TrackView> = new Map();
   private dataStore: DataStore<Series> = new DataStore();
 
   constructor(helicorder: Helicorder) {
     this.helicorder = helicorder;
   }
 
+  /**
+   * Get the data store for the tracks.
+   */
   getDataStore(): DataStore<Series> {
     return this.dataStore;
   }
 
+  /**
+   * Add a track to the manager for the given segment.
+   */
   add(segment: Segment, track: TrackView): void {
     const key = JSON.stringify(segment);
-    this._tracks.set(key, track);
+    this.trackStore.set(key, track);
   }
 
+  /**
+   * Remove a track from the manager for the given segment.
+   */
   remove(segment: Segment): void {
     const key = JSON.stringify(segment);
-    this._tracks.delete(key);
+    this.trackStore.delete(key);
   }
 
+  /**
+   * Get the track for the given segment.
+   */
   get(segment: Segment): TrackView | undefined {
     const key = JSON.stringify(segment);
-    return this._tracks.get(key);
+    return this.trackStore.get(key);
   }
 
+  /**
+   * Check if the manager has a track for the given segment.
+   */
   has(segment: Segment): boolean {
     const key = JSON.stringify(segment);
-    return this._tracks.has(key);
+    return this.trackStore.has(key);
   }
 
+  /**
+   * Clear all tracks and data from the manager.
+   */
   clear(): void {
-    this._tracks.clear();
+    this.trackStore.clear();
+    this.clearData();
   }
 
+  /**
+   * Get the number of tracks in the manager. Note that this is not the same as
+   * the number of segments in the data store. The number of tracks is
+   * determined by the duration and interval of the helicorder sufficient to
+   * display the data.
+   */
   count(): number {
     const model = this.helicorder.getModel();
     const { interval, duration } = model.getOptions();
     return Math.ceil((duration * 60) / interval) + 1;
-  }
-
-  getTrackIndexByTime(time: number): number {
-    const model = this.helicorder.getModel();
-    const { interval, offsetDate } = model.getOptions();
-    const segment = interval * ONE_MINUTE;
-    const endOf = (value: number) => value + segment - (value % segment);
-    const end = endOf(offsetDate);
-    const diff = end - time;
-    return Math.floor(diff / segment);
   }
 
   /**
@@ -73,6 +91,22 @@ export class TrackManager {
     return this.getRectForTrack(trackIndex, this.count() - 1);
   }
 
+  /**
+   * Get the track index for the given time.
+   */
+  getTrackIndexByTime(time: number): number {
+    const model = this.helicorder.getModel();
+    const { interval, offsetDate } = model.getOptions();
+    const segment = interval * ONE_MINUTE;
+    const endOf = (value: number) => value + segment - (value % segment);
+    const end = endOf(offsetDate);
+    const diff = end - time;
+    return Math.floor(diff / segment);
+  }
+
+  /**
+   * Get the track index for the given position.
+   */
   getTrackIndexByPosition(y: number): number {
     const { y: gridY, height } = this.helicorder.getGrid().getRect();
     const trackHeight = height / this.count();
@@ -80,16 +114,25 @@ export class TrackManager {
     return this.count() - row - 1;
   }
 
+  /**
+   * Get the track for the given index.
+   */
   getTrackByIndex(index: number): TrackView | undefined {
     const segment = this.getTrackExtentAt(index);
     return this.get(segment);
   }
 
+  /**
+   * Get the track for the given time.
+   */
   getTrackByTime(time: number): TrackView | undefined {
     const index = this.getTrackIndexByTime(time);
     return this.getTrackByIndex(index);
   }
 
+  /**
+   * Get the track for the given Y position.
+   */
   getTrackByPosition(y: number): TrackView | undefined {
     const { y: gridY, height } = this.helicorder.getGrid().getRect();
     const trackHeight = height / this.count();
@@ -98,6 +141,9 @@ export class TrackManager {
     return this.get(segment);
   }
 
+  /**
+   * Get the extent of the track, i.e. segment at the given index.
+   */
   getTrackExtentAt(index: number): [number, number] {
     const model = this.helicorder.getModel();
     const { interval, offsetDate } = model.getOptions();
@@ -112,6 +158,9 @@ export class TrackManager {
     ];
   }
 
+  /**
+   * Get the extents or segments of all the tracks.
+   */
   getTrackExtents(): [number, number][] {
     const extents: [number, number][] = [];
     for (let i = 0; i < this.count(); i++) {
@@ -121,12 +170,20 @@ export class TrackManager {
     return extents;
   }
 
+  /**
+   * Get the extent of the chart, i.e. the first and last segment.
+   */
   getChartExtent(): [number, number] {
     const last = this.getTrackExtentAt(0);
     const first = this.getTrackExtentAt(this.count() - 1);
     return [first[0], last[1]];
   }
 
+  /**
+   * Convert the time to offset in the track. For example, if the segment is
+   * [start, end] and the time is in between, the offset will be the fraction of
+   * the interval, i.e. (time - start) / (end - start).
+   */
   timeToOffset(segment: Segment, time: number): number {
     const model = this.helicorder.getModel();
     const { interval } = model.getOptions();
@@ -134,6 +191,9 @@ export class TrackManager {
     return ((time - start) / (end - start)) * interval;
   }
 
+  /**
+   * Get the time at the given point in the helicorder chart.
+   */
   getTimeAtPoint(x: number, y: number): number {
     const trackIndex = this.getTrackIndexByPosition(y);
     const [start, end] = this.getTrackExtentAt(trackIndex);
@@ -154,20 +214,29 @@ export class TrackManager {
     return this.helicorder.getXAxis().getPixelForValue(offsetSeconds);
   }
 
+  /**
+   * Iterate over all the segments in the manager.
+   */
   *segments(): Generator<Segment> {
     for (let i = 0; i < this.count(); i++) {
       yield this.getTrackExtentAt(i);
     }
   }
 
+  /**
+   * Iterate over all the tracks in the manager.
+   */
   *tracks(): Generator<TrackView> {
-    for (const track of this._tracks.values()) {
+    for (const track of this.trackStore.values()) {
       yield track;
     }
   }
 
+  /**
+   * Iterate over all the items in the manager.
+   */
   *items(): Generator<[Segment, TrackView]> {
-    for (const [key, track] of this._tracks.entries()) {
+    for (const [key, track] of this.trackStore.entries()) {
       yield [JSON.parse(key), track];
     }
   }
@@ -185,10 +254,17 @@ export class TrackManager {
     return isFinite(normFactor) ? normFactor : 1;
   }
 
+  /**
+   * Set the data for the given segment.
+   */
   setTrackData(segment: Segment, data: Series): void {
     this.dataStore.set(segment, data);
   }
 
+  /**
+   * Get the data for the given segment. If the data is not available, an empty
+   * series is returned.
+   */
   getTrackData(segment: Segment): Series {
     const data = this.dataStore.get(segment);
     if (!data) {
@@ -197,11 +273,17 @@ export class TrackManager {
     return data;
   }
 
+  /**
+   * Check if the data for the given segment is empty.
+   */
   isTrackEmpty(segment: Segment): boolean {
     const heliData = this.dataStore.get(segment);
     return !heliData || heliData.length === 0;
   }
 
+  /**
+   * Slice the data from start to end for the given segment.
+   */
   sliceData(segment: Segment, start: number, end: number): Series {
     const series = this.dataStore.get(segment);
     if (!series) {
@@ -218,10 +300,17 @@ export class TrackManager {
       this.getTrackIndexByTime(start + (end - start) / 2)
     );
     const series = this.sliceData(segment, start, end);
-
     return [series.min(), series.max()];
   }
 
+  /**
+   * Refresh the data for all the tracks. The data is refreshed based on the
+   * scaling option set in the helicorder model. If the scaling is set to
+   * "global", the data is scaled based on the global min and max values of all
+   * the tracks. If the scaling is set to "local", the data is scaled based on
+   * the min and max values of the individual tracks. If you use offscreen
+   * rendering, you don't need to call this method.
+   */
   refreshData(): void {
     const { scaling } = this.helicorder.getModel().getOptions();
     if (scaling === "global") {
@@ -267,6 +356,9 @@ export class TrackManager {
     }
   }
 
+  /**
+   * Clear the data for all the tracks.
+   */
   clearData(): void {
     this.dataStore.clear();
     for (const track of this.tracks()) {
@@ -274,6 +366,11 @@ export class TrackManager {
     }
   }
 
+  /**
+   * Update the track labels based on the time and the number of tracks. You may
+   * want to call this method when timezone changes or when the number of tracks
+   * changes.
+   */
   updateTrackLabels(): void {
     const model = this.helicorder.getModel();
     const { useUTC } = model.getOptions();
@@ -318,7 +415,7 @@ export class TrackManager {
   }
 
   /**
-   * Get the rectange for the track at the given index.
+   * Get the rectangle for the track at the given index.
    */
   getRectForTrack(index: number, count: number): LayoutRect {
     const grid = this.helicorder.getGrid();
@@ -334,6 +431,11 @@ export class TrackManager {
     return new BoundingRect(x, trackY, width, trackHeight);
   }
 
+  /**
+   * Update the tracks in the manager. This method should be called when there
+   * is a change in the the offset date, interval, or duration of the
+   * helicorder.
+   */
   updateTracks(): void {
     for (const track of this.tracks()) {
       this.helicorder.removeComponent(track);
@@ -355,6 +457,10 @@ export class TrackManager {
     this.refreshData();
   }
 
+  /**
+   * Resize the tracks in the manager. This method should be called when the
+   * size of the helicorder changes.
+   */
   resizeTracks(): void {
     for (const segment of this.segments()) {
       const track = this.get(segment);
