6650ae56b585b93988665c6bf17245e5f7351cdf
Indra Rudianto <indrarudianto.official@gmail.com>
2024-10-20 13:49:46 +0700

    Update helicorder event map and add docstring

--- packages/zcharts/src/helicorder/eventMap.ts
+++ packages/zcharts/src/helicorder/eventMap.ts
@@ -2,14 +2,28 @@ import { Channel, EventMap } from "../util/types";
 import { EventMarkerOptions } from "./eventMarker/eventMarkerModel";
 
 export interface HelicorderEventMap extends EventMap {
+  /**
+   * Fired when the channel changes.
+   */
   channelChanged: (channel: Channel) => void;
+  /**
+   * Fired when the offset date changes.
+   */
   offsetChanged: (offset: number) => void;
-  intervalChanged: (interval: number) => void;
-  durationChanged: (duration: number) => void;
+  /**
+   * Fired when the amplitude range changes.
+   */
   amplitudeChanged: (range: [number, number]) => void;
-  trackSelected: (index: number) => void;
-  trackDeselected: () => void;
+  /**
+   * Fired when selection window changes.
+   */
   selectionChanged: (range: [number, number]) => void;
+  /**
+   * Fired when the event marker is clicked.
+   */
   eventMarkerClicked: (marker: EventMarkerOptions) => void;
+  /**
+   * Fired when the loading state changes.
+   */
   loading: (loading: boolean) => void;
 }
