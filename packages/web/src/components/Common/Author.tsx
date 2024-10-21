42e8716688a90a8a5ede9ce7a17288c27285632e
Indra Rudianto <indrarudianto.official@gmail.com>
2024-10-21 15:22:19 +0700

    Refactor avatar URL in Author component

--- packages/web/src/components/Common/Author.tsx
+++ packages/web/src/components/Common/Author.tsx
@@ -1,13 +1,17 @@
 import { Avatar, AvatarSize, Tooltip } from '@fluentui/react-components';
+import { baseUrl } from '../../services/api';
 import { User } from '../../types/user';
 
 const Author: React.FC<{ author?: User; size?: AvatarSize }> = ({ author, size = 32 }) => {
   if (!author) {
     return null;
   }
+
+  const avatarUrl = author.avatar && !author.avatar.startsWith('http') ? `${baseUrl}${author.avatar}` : author.avatar;
+
   return (
     <Tooltip content={author.name || author.username} relationship="label">
-      <Avatar size={size} aria-label={author.name} name={author.name} color="colorful" image={{ src: author.avatar }} />
+      <Avatar size={size} aria-label={author.name} name={author.name} color="colorful" image={{ src: avatarUrl }} />
     </Tooltip>
   );
 };
