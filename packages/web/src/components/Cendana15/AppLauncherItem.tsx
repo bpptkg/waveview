2a15f4395be19a5fc90e021a08b8110acd7b2f5a
Indra Rudianto <indrarudianto.official@gmail.com>
2024-10-29 13:00:23 +0700

    Refactor AppLauncherItem component to add cursor pointer

--- packages/web/src/components/Cendana15/AppLauncherItem.tsx
+++ packages/web/src/components/Cendana15/AppLauncherItem.tsx
@@ -8,7 +8,7 @@ export interface AppLauncherItemProps {
 
 const AppLauncherItem: React.FC<AppLauncherItemProps> = ({ href, src, title }) => {
   return (
-    <li className="w-[86px] h-[86px] pt-1 relative rounded-md hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-300 group">
+    <li className="w-[86px] h-[86px] pt-1 relative rounded-md hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-300 group cursor-pointer">
       <a href={href} className="w-[86px]">
         <div className="mb-1 flex items-center justify-center">
           <img src={src} alt={title} className="w-[40px] h-[40px] rounded-md"></img>
