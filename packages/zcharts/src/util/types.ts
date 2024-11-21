ab9586b392043780cf85ffe6c70aa44e2f20cc77
Indra Rudianto <indrarudianto.official@gmail.com>
2024-11-21 11:37:17 +0700

    Enhance type definitions with detailed comments

--- packages/zcharts/src/util/types.ts
+++ packages/zcharts/src/util/types.ts
@@ -1,3 +1,4 @@
+import { Series } from "@waveview/ndarray";
 import { BoundingRect } from "zrender";
 
 export type ModelOptions = Record<string, any>;
@@ -39,52 +40,139 @@ export interface ThemeRegistry {
 export type ThemeName = keyof ThemeRegistry;
 
 export interface AxisStyle {
+  /**
+   * The color of the axis line.
+   */
   axisLineColor: string;
+  /**
+   * The color of the axis tick.
+   */
   axisTickColor: string;
+  /**
+   * The color of the axis split line.
+   */
   splitLineColor: string;
 }
 
 export interface GridStyle {
+  /**
+   * The color of the grid line.
+   */
   lineColor: string;
+  /**
+   * The width of the grid line.
+   */
   lineWidth: number;
 }
 
 export interface SeriesStyle {
+  /**
+   * The color of the series line.
+   */
   lineColor: string;
+  /**
+   * The width of the series line.
+   */
   lineWidth: number;
 }
 
 export interface HighlightStyle {
+  /**
+   * The color of the highlight area.
+   */
   color: string;
+  /**
+   * The opacity of the highlight area.
+   */
   opacity: number;
+  /**
+   * The width of the highlight border.
+   */
   borderWidth: number;
 }
 
 export interface AxisPointerStyle {
+  /**
+   * The color of the axis pointer line.
+   */
   lineColor: string;
+  /**
+   * The width of the axis pointer line.
+   */
   lineWidth: number;
+  /**
+   * The color of the axis pointer label.
+   */
   textColor: string;
+  /**
+   * The font size of the axis pointer label.
+   */
   fontSize: number;
+  /**
+   * The background color of the axis pointer label.
+   */
   backgroundColor: string;
 }
 
 export interface PickerStyle {
+  /**
+   * The color of the picker area.
+   */
   color: string;
+  /**
+   * The opacity of the picker area.
+   */
   opacity: number;
+  /**
+   * The width of the picker border.
+   */
   borderWidth: number;
 }
 
 export interface ThemeStyle {
+  /**
+   * The background color of the chart.
+   */
   backgroundColor: string;
+  /**
+   * The color of the chart foreground.
+   */
   foregroundColor: string;
+  /**
+   * The color of the chart text.
+   */
   textColor: string;
+  /**
+   * The font size of the chart text.
+   */
   fontSize: number;
+  /**
+   * The font family of the chart text.
+   */
   fontFamily: string;
+  /**
+   * The grid style of the chart.
+   */
   gridStyle: GridStyle;
+  /**
+   * The axis style of the chart.
+   */
   axisStyle: AxisStyle;
+  /**
+   * The series style of the chart.
+   */
   seriesStyle: SeriesStyle;
+  /**
+   * The highlight style of the chart.
+   */
   highlightStyle: HighlightStyle;
+  /**
+   * The axis pointer style of the chart.
+   */
   axisPointerStyle: AxisPointerStyle;
+  /**
+   * The picker style of the chart.
+   */
   pickerStyle: PickerStyle;
 }
 
@@ -109,4 +197,71 @@ export interface Channel {
 
 export interface AddMarkerOptions {
   show?: boolean;
+}
+
+export interface SeriesData {
+  /**
+   * The series data.
+   */
+  series: Series;
+  /**
+   * Minimum value of the series.
+   */
+  min: number;
+  /**
+   * Maximum value of the series.
+   */
+  max: number;
+  /**
+   * Mean value of the series.
+   */
+  mean: number;
+  /**
+   * Count of the series.
+   */
+  count: number;
+}
+
+export interface SpectrogramData {
+  /**
+   * The spectrogram image data. The image data is stored as a 1D array of 8-bit
+   * unsigned integers. The image data is stored in row-major order.
+   */
+  image: Uint8Array;
+  /**
+   * The minimum time value of the spectrogram in Unix time.
+   */
+  timeMin: number;
+  /**
+   * The maximum time value of the spectrogram in Unix time.
+   */
+  timeMax: number;
+  /**
+   * The minimum frequency value of the spectrogram.
+   */
+  freqMin: number;
+  /**
+   * The maximum frequency value of the spectrogram.
+   */
+  freqMax: number;
+  /**
+   * The number of time bins in the spectrogram.
+   */
+  timeLength: number;
+  /**
+   * The number of frequency bins in the spectrogram.
+   */
+  freqLength: number;
+  /**
+   * The minimum value of the spectrogram.
+   */
+  min: number;
+  /**
+   * The maximum value of the spectrogram.
+   */
+  max: number;
+  /**
+   * The URL of the spectrogram image.
+   */
+  imageURL: string;
 }
