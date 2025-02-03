1bf6572b5fca200596f1e4728ddd17c1f8779724
Indra Rudianto <indrarudianto.official@gmail.com>
2025-02-03 14:33:47 +0700

    Refine layout of KeyValuePair component for improved spacing and alignment

--- packages/web/src/components/Common/KeyValuePair.tsx
+++ packages/web/src/components/Common/KeyValuePair.tsx
@@ -8,9 +8,9 @@ interface KeyValuePairProps {
 
 const KeyValuePair: React.FC<KeyValuePairProps> = ({ label, value, column }) =>
   column ? (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-1">
       <div>{label}</div>
-      <div>{value}</div>
+      <div className="ml-2">{value}</div>
     </div>
   ) : (
     <div className="flex items-center justify-between hover:bg-gray-100 dark:hover:bg-gray-800">
