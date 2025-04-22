4eee2e2b0cfd442aeca39e234c2d859fd5730309
Indra Rudianto <indrarudianto.official@gmail.com>
2025-04-22 21:20:21 +0700

    feat: Enhance AppLauncher tooltip with icon and improved layout

--- packages/web/src/components/Cendana15/AppLauncher.tsx
+++ packages/web/src/components/Cendana15/AppLauncher.tsx
@@ -1,10 +1,19 @@
 import { Tooltip } from '@fluentui/react-components';
-import { AppFolderRegular } from '@fluentui/react-icons';
+import { AppFolderRegular, ArrowUpRightRegular } from '@fluentui/react-icons';
 import React from 'react';
 
 const AppLauncher: React.FC = () => {
   return (
-    <Tooltip content={'Cendana15 Apps'} relationship="label" showDelay={1500}>
+    <Tooltip
+      content={
+        <div className="flex items-center gap-2">
+          <span>Cendana15 Apps</span>
+          <ArrowUpRightRegular />
+        </div>
+      }
+      relationship="label"
+      showDelay={1500}
+    >
       <a href="https://cendana15.com" target="_blank" rel="noreferrer" className="flex items-center justify-center">
         <AppFolderRegular fontSize={20} />
       </a>
